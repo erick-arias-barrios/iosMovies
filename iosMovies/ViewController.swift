01be1c99a301a2e0f4392734d917cb6a38098d44
Erick Arias <erick.arias.barrios@gmail.com>
2022-08-01 18:37:58 -0500

    Initial Commit

---
+++ iosMovies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iosMovies
+//
+//  Created by Erick Arias on 1/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
