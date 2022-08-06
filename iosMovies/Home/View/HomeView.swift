8ae5b7451e88999691999a963920ca8d9b2e55a8
Erick Arias <erick.arias.barrios@gmail.com>
2022-08-05 21:33:22 -0500

    Con el gestor de dependencias Cocoapods, se agrego RxSwift y RxCocoa. Se modifico los archivos HomeView.xib, y HomeView.swift.

--- iosMovies/Home/View/HomeView.swift
+++ iosMovies/Home/View/HomeView.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class HomeView: UIViewController {
     
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var activity: UIActivityIndicatorView!
+    
     private var router = HomeRouter()
     private var viewModel = HomeViewModel()
 
@@ -17,4 +21,15 @@ class HomeView: UIViewController {
         viewModel.bind(view: self, router: router)
     }
 
+}
+
+extension HomeView: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
+        return cell
+    }
 }
